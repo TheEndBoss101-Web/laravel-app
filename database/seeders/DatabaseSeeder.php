809cf24c3ceeb7f737571849cb080925d85b3c7e
theendboss101 <theendboss101@gmail.com>
2025-04-13 23:37:57 -0500

    feat: update DatabaseSeeder to create an Administrator user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        //$this->call(LaratrustSeeder::class);
+
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        //User::factory()->create([
+        //    'name' => 'Test User',
+        //    'email' => 'test@example.com',
+        //]);
+        $user = User::factory()->create([
+            'name' => 'Administrator',
+            'email' => 'Administrator@example.com',
+            'password' => bcrypt('password'),
         ]);
+        //$user->addRole("superadministrator");
+        //$user->roles()->attach([1]);
     }
 }
