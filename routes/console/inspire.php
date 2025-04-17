848b9b2c25c003845e025fad2300edc16a20d921
TheEndBoss_101 <94846253+TheEndBoss101@users.noreply.github.com>
2025-04-17 13:51:49 -0500

    chore: move inspire cmd to own route folder

---
+++ routes/console/inspire.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+
+/*
+|--------------------------------------------------------------------------
+| Inspire
+|--------------------------------------------------------------------------
+|
+| Display an inspiring quote.
+|
+*/
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
