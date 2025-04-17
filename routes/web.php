cf1a06c266ae7bf9390e9e4126841133d09dc9c4
TheEndBoss_101 <94846253+TheEndBoss101@users.noreply.github.com>
2025-04-17 13:47:18 -0500

    format: add comments to route files

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Env;
 use Inertia\Inertia;
-
 use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::fallback(function () {
     abort(404);
